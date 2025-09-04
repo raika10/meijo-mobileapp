49b6b11c4838a1116611a8ed16c04bd0eb01dbde
Raika Morita <[raika10]+[>
2025-09-04 14:12:57 +0900

    画像へアクセスできるようにした。

---
+++ app/src/main/java/jp/ac/meijou/android/s241205171/MainActivity6.java
@@ -0,0 +1,74 @@
+package jp.ac.meijou.android.s241205171;
+
+import android.graphics.BitmapFactory;
+import android.net.ConnectivityManager;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.io.IOException;
+import java.util.Optional;
+
+import jp.ac.meijou.android.s241205171.databinding.ActivityMain6Binding;
+import jp.ac.meijou.android.s241205171.databinding.ActivityMainBinding;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class MainActivity6 extends AppCompatActivity {
+
+    private ActivityMain6Binding binding;
+
+    private final OkHttpClient okHttpClient = new OkHttpClient();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        binding = ActivityMain6Binding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        binding.button8.setOnClickListener(view -> {
+            var text = binding.editTextText2.getText().toString();
+
+            var url = Uri.parse("https://placehold.jp/500x500.png")
+                    .buildUpon()
+                    .appendQueryParameter("text", text)
+                    .build()
+                    .toString();
+
+            getImage(url);
+        });
+    }
+
+    private void getImage(String url) {
+        var request = new Request.Builder()
+                .url(url)
+                .build();
+
+        okHttpClient.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(@NonNull Call call, @NonNull IOException e) {
+            }
+
+            @Override
+            public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+                var bitmap = BitmapFactory.decodeStream(response.body().byteStream());
+                runOnUiThread(() -> binding.imageView2.setImageBitmap(bitmap));
+            }
+        });
+    }
+}
